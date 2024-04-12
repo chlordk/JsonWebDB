01f753d50d56c235da1e13bdf04979aa43810f40
ahoeffner <alex@hoeffner.net>
2024-04-12 18:47:28 +0200

    .

---
+++ jsondb/src/main/java/jsondb/messages/Messages.java
@@ -0,0 +1,69 @@
+/*
+MIT License
+
+Copyright (c) 2024 Alex Høffner
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package jsondb.messages;
+
+import java.io.File;
+import jsondb.Config;
+import java.util.Locale;
+import java.io.FileInputStream;
+import java.util.PropertyResourceBundle;
+
+
+public class Messages
+{
+   private static final String folder = "messages";
+   private static final String bundle = "Messages";
+   private static final PropertyResourceBundle messages = load();
+
+
+   public static String get(String name) throws Exception
+   {
+      if (messages == null) return("?");
+      else return(messages.getString(name));
+   }
+
+   private static PropertyResourceBundle load()
+   {
+      PropertyResourceBundle messages = null;
+
+      try
+      {
+         String path = Config.path(folder,bundle);
+         String lang = Locale.getDefault().getLanguage();
+
+         lang = lang.toUpperCase();
+         File location = new File(path+lang+".msg");
+         if (!location.exists()) location = new File(path+"EN.msg");
+
+         messages = new PropertyResourceBundle(new FileInputStream(location));
+      }
+      catch (Exception e)
+      {
+         e.getStackTrace();
+      }
+
+      return(messages);
+   }
+}
