98b56be8d3bc28c839ee220022437f65fb195cee
ahoeffner <alex@hoeffner.net>
2024-04-16 22:24:08 +0200

    .

--- jsondb/src/main/java/http/Server.java
+++ jsondb/src/main/java/http/Server.java
@@ -24,21 +24,78 @@ SOFTWARE.
 
 package http;
 
+import java.net.URL;
+import java.io.File;
 import jsondb.JsonDB;
+import java.io.FilenameFilter;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpsServer;
 
 
 public class Server
 {
    public static void main(String[] args) throws Exception
    {
-      JsonDB.initialize(args[0],args[1]);
+      if (args.length != 1)
+      {
+         System.err.println("Program requires exactly one argument [instance name]");
+         System.exit(-1);
+      }
+
+      String inst = args[0];
+      String root = findAppHome();
+      JsonDB.initialize(root,inst);
 
       HttpServer server = HttpServer.create(new InetSocketAddress("localhost",6001),16);
       server.setExecutor(Executors.newFixedThreadPool(16));
       server.createContext("/",new Handler());
       server.start();
+
+      //HttpsServer secsrv = HttpsServer.create(null,0);
+   }
+
+   private static String findAppHome()
+   {
+      Object obj = new Object() { };
+
+      String cname = obj.getClass().getEnclosingClass().getName();
+      cname = "/" + cname.replace('.','/') + ".class";
+
+      URL url = obj.getClass().getResource(cname);
+      String path = url.getPath();
+
+      if (url.getProtocol().equals("jar") || url.getProtocol().equals("code-source"))
+      {
+         path = path.substring(5); // get rid of "file:"
+         path = path.substring(0,path.indexOf("!")); // get rid of "!class"
+         path = path.substring(0,path.lastIndexOf("/")); // get rid of jarname
+      }
+
+      int pos = 0;
+      File folder = null;
+      HasConfigFolder check = new HasConfigFolder();
+
+      while(pos >= 0)
+      {
+         folder = new File(path);
+
+         if (folder.isDirectory() && folder.listFiles(check).length == 1)
+            break;
+
+         pos = path.lastIndexOf("/");
+         path = path.substring(0,pos);
+      }
+
+      return(path);
+   }
+
+
+   private static class HasConfigFolder implements FilenameFilter
+   {
+      @Override
+      public boolean accept(File dir, String name)
+         {return(name.equals("config"));}
    }
 }
